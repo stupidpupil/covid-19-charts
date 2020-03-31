19cce68349a046c8b0fe6ff20233129ba0e44e4f
Adam Watkins <adam@stupidpupil.co.uk>
2020-03-31 16:24:45 +0100

    Get some deaths

--- get_some_data.R
+++ get_some_data.R
@@ -31,3 +31,29 @@ cases_by_nhs <- cases_by_nhs %>% left_join(orgs, by="Health Board/NHS Region")
 cases_by_nhs$`Health Board/NHS Region` = 
   factor(cases_by_nhs$`Health Board/NHS Region`, levels = orgs$`Health Board/NHS Region`)
 
+
+#
+# Deaths
+#
+
+min_deaths = 20
+
+deaths_by_country <- read_csv("https://docs.google.com/spreadsheets/d/1LYQ7sz8GEpS2ClwuGuu_-EIiy5x2miycsWIVVlXrZoI/export?format=csv&id=1LYQ7sz8GEpS2ClwuGuu_-EIiy5x2miycsWIVVlXrZoI&gid=2034844950")
+
+deaths_by_country <- deaths_by_country %>% 
+  mutate(Date = dmy(Date)) %>%
+  pivot_longer(-Date, names_to = "Country", values_to = "Deaths")
+
+deaths_by_country <- deaths_by_country %>%
+  group_by(Country) %>%
+  mutate(
+    DaysSinceMinDeaths = case_when(
+        Deaths < min_deaths ~ -1,
+        TRUE ~ 0
+      )
+    ) %>%
+  filter(DaysSinceMinDeaths >= 0, !is.na(Deaths)) %>%
+  mutate(
+    DaysSinceMinDeaths = as.numeric(Date-min(Date), unit="days")
+    ) %>%
+  ungroup()
