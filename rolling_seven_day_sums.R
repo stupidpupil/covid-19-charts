dcc5a7a7c9a48078d86fa7102f277743856ea96c
Adam Watkins <adam@stupidpupil.co.uk>
2020-04-03 00:14:09 +0100

    Don't treat trailing missing data as zero

--- rolling_seven_day_sums.R
+++ rolling_seven_day_sums.R
@@ -9,10 +9,15 @@ add_rolling_seven_day_sums <- function(data, col_to_be_summed, group, date = Dat
   q_group = enquo(group)
   q_date = enquo(date)
 
+  replace_non_trailing_na <- function(x, y){
+    trailing_na_bool <- (rev(cumsum(!is.na(rev(x)))) == 0)
+    ifelse(trailing_na_bool, NA, ifelse(is.na(x), y, x))
+  }
+
   data %>%
     complete(!!q_group, !!q_date := full_seq(!!q_date, 1)) %>%
     group_by(!!q_group) %>%
     mutate(
-      !!new_col_name := roll_sum(!!enquo(col_to_be_summed) %>% replace_na(0), 7, align = "right", fill = NA)
+      !!new_col_name := roll_sum(!!enquo(col_to_be_summed) %>% replace_non_trailing_na(0), 7, align = "right", fill = NA)
     )
 }
