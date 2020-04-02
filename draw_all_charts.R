aa34bcb07c219813d839c9d0e1ee2a134848777d
Adam Watkins <adam@stupidpupil.co.uk>
2020-04-02 18:16:51 +0100

    Rename chart scripts

--- draw_all_charts.R
+++ draw_all_charts.R
@@ -1,6 +1,6 @@
 source("get_some_data.R")
-source("draw_a_chart.R")
-source("draw_a_deaths_chart.R")
-source("draw_a_new_cases_in_last_week_chart.R")
-source("draw_a_new_deaths_last_week_chart.R")
+source("draw_a_chart_of_cumulative_cases.R")
+source("draw_a_chart_of_cumulative_deaths.R")
+source("draw_a_chart_of_new_cases_in_last_week.R")
+source("draw_a_chart_of_new_deaths_in_last_week.R")
 
