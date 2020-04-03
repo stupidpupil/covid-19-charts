7ba0f3357ab6a012b120797914b3aebd02518bbc
Adam Watkins <adam@stupidpupil.co.uk>
2020-04-03 10:13:05 +0100

    Add TODO

--- days_since_min.R
+++ days_since_min.R
@@ -12,7 +12,7 @@ add_days_since_min <- function(data, col, group, min = 10, date = Date, prefix =
       !!q_col < min ~ -1,
       TRUE ~ 0
     )) %>%
-    filter(!is.na(!!q_col), !!new_col_name == 0) %>%
+    filter(!is.na(!!q_col), !!new_col_name == 0) %>% # TODO: Make this non-destructive
     mutate(
       !!new_col_name := as.numeric(!!q_date - min(!!q_date), unit = "days")
     ) %>%
