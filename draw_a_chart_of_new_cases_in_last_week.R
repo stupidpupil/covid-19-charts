4be0c225889d15e7f02d68a02c849f28ade49645
Adam Watkins <adam@stupidpupil.co.uk>
2020-04-04 16:31:30 +0100

    Fixed ordering of Regions and height

--- draw_a_chart_of_new_cases_in_last_week.R
+++ draw_a_chart_of_new_cases_in_last_week.R
@@ -6,18 +6,17 @@ source("draw_a_chart_function.R")
 # HACK: we don't have data before 9th Mar, so we miss when some regions actually first
 # had more than 100 cases-per-week. This is a particular problem for London, so we guess
 # that actually London excedded the threshold 5 days earlier.
-for_new_cases_in_last_week_chart <- for_new_cases_in_last_week_chart %>% mutate(
+mod_for_new_cases_in_last_week_chart <- for_new_cases_in_last_week_chart %>% mutate(
     DaysSinceMinNewCasesInLastWeek = case_when(
-      Region == 'London' ~ DaysSinceMinNewCasesInLastWeek + 6,
+      Region == "London" ~ DaysSinceMinNewCasesInLastWeek + 6,
       TRUE ~ DaysSinceMinNewCasesInLastWeek
     )
   )
 
-comments <- tibble(Region="London", text="Offset by 6 days")
+comments <- tibble(Region=factor("London", levels=levels(mod_for_new_cases_in_last_week_chart$Region)), text="Offset by 6 days")
 
 
-
-my_plot <- for_new_cases_in_last_week_chart %>% draw_a_jburnish_chart(
+my_plot <- mod_for_new_cases_in_last_week_chart %>% draw_a_jburnish_chart(
   x = DaysSinceMinNewCasesInLastWeek, y = NewCasesInLastWeek, group = Region, y_min = min_new_cases_in_last_week, y_max = 5100,
   title = paste0("New confirmed COVID-19 cases in previous week, \nby days-since-", min_new_cases_in_last_week, "-new-cases-first-confirmed-in-a-week"),
   y_title = "New confirmed cases of COVID-19 in previous 7 days",
@@ -32,5 +31,5 @@ print(my_plot)
 scaling_factor <- 1.56
 ggsave(
   paste0("output/", pretty_max_date, " New Cases in Last Week", ".png"),
-  height = 11 / (1.1 * scaling_factor), width = 11.5 / scaling_factor, dpi = 72 * scaling_factor
+  height = 11 / scaling_factor, width = 11.5 / scaling_factor, dpi = 72 * scaling_factor
 )
